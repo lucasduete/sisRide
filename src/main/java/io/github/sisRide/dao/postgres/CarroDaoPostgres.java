55fbb0860fac59e155da618122f97f2edddfff69
Lucas Duete <lm.duete@gmail.com>
2018-02-25 16:50:03 -0300

    Finalizando Implementaçao de CarroDaoPostgres.

--- src/main/java/io/github/sisRide/dao/postgres/CarroDaoPostgres.java
+++ src/main/java/io/github/sisRide/dao/postgres/CarroDaoPostgres.java
@@ -71,6 +71,10 @@ public class CarroDaoPostgres {
                 carros.add(carro);
             }
 
+            rs.close();
+            stmt.close();
+            conn.close();
+
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
@@ -78,6 +82,122 @@ public class CarroDaoPostgres {
         return carros;
     }
 
+    public boolean atualizar(Carro carro) {
 
+        String sql = "UPDATE Carro SET Placa = ?, Modelo = ?, ArCondicionado = ?, Ano = ?, " +
+                "EmailMotorista = ? WHERE Placa = ?";
+
+        try {
+            PreparedStatement stmt = conn.prepareStatement(sql);
+
+            stmt.setString(1, carro.getPlaca());
+            stmt.setString(2, carro.getModelo());
+            stmt.setBoolean(3, carro.getArCondicionado());
+            stmt.setDate(4, Date.valueOf(carro.getAno()));
+            stmt.setString(5, carro.getEmailAdministrador());
+            stmt.setString(6, carro.getPlaca());
+
+            stmt.executeUpdate();
+
+            stmt.close();
+            conn.close();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            return false;
+        }
+
+        return true;
+    }
+
+    public boolean deletar(Carro carro) {
+        String sql = "DELETE FROM Carro WHERE Placa = ?";
+
+        try {
+            PreparedStatement stmt = conn.prepareStatement(sql);
+
+            stmt.setString(1, carro.getPlaca());
+
+            stmt.executeUpdate();
+
+            stmt.close();
+            conn.close();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            return false;
+        }
+
+        return true;
+    }
+
+    public Carro getCarroByPlaca(String placa) {
+
+        String sql = "SELECT * FROM Carro WHERE Placa = ?;";
+        Carro carro = null;
+
+        try {
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            carro = new Carro();
+
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                Date data = rs.getDate("Ano");
+                Instant instant = Instant.ofEpochMilli(data.getTime());
+
+                carro.setPlaca(rs.getString("Placa"));
+                carro.setModelo(rs.getString("Modelo"));
+                carro.setArCondicionado(rs.getBoolean("ArCondicionado"));
+                carro.setAno(
+                        LocalDateTime.ofInstant(instant, ZoneId.systemDefault()).toLocalDate()
+                );
+                carro.setEmailAdministrador(rs.getString("EmailMotorista"));
+            }
+
+            rs.close();
+            stmt.close();
+            conn.close();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+        return carro;
+    }
+
+    public List<Carro> getCarrosByMotorista(String emailMotorista) {
+        String sql = "SELECT Placa, Modelo, ArCondicionado, Ano FROM Carro " +
+                "WHERE EmailMotorista = ?;";
+        List<Carro> carros = new ArrayList<>();
+
+        try {
+            PreparedStatement stmt = conn.prepareStatement(sql);
+
+            stmt.setString(1, emailMotorista);
+
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                Date data = rs.getDate("Ano");
+                Instant instant = Instant.ofEpochMilli(data.getTime());
+
+                Carro carro = new Carro(
+                        rs.getString("Placa"),
+                        rs.getString("Modelo"),
+                        rs.getBoolean("ArCondicionado"),
+                        LocalDateTime.ofInstant(instant, ZoneId.systemDefault()).toLocalDate(),
+                        rs.getString(emailMotorista)
+                );
+
+                carros.add(carro);
+            }
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+        return carros;
+    }
 
 }
