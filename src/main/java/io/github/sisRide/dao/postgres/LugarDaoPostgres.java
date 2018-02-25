ccb4c362c8ee6755b6b66efc60b06c081726cb5e
Lucas Duete <lm.duete@gmail.com>
2018-02-25 17:08:35 -0300

    Finalizando Implementacao do LugarDaoPostgres.

--- src/main/java/io/github/sisRide/dao/postgres/LugarDaoPostgres.java
+++ src/main/java/io/github/sisRide/dao/postgres/LugarDaoPostgres.java
@@ -79,5 +79,86 @@ public class LugarDaoPostgres {
         return lugares;
     }
 
+    public boolean atualizar(Lugar lugar) {
+        String sql = "UPDATE Lugar SET Descricao = ?, Nome = ?, Lat = ?, Lng = ?, EmailUsuario = ? " +
+                "WHERE Place_Id = ?";
+
+        try {
+            PreparedStatement stmt = conn.prepareStatement(sql);
+
+            stmt.setString(1, lugar.getDescricao());
+            stmt.setString(2, lugar.getNome());
+            stmt.setFloat(3, lugar.getLat());
+            stmt.setFloat(3, lugar.getLng());
+            stmt.setString(4, lugar.getEmailUsuario());
+
+            stmt.executeUpdate();
+
+            stmt.close();
+            conn.close();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            return false;
+        }
+
+        return true;
+    }
+
+    public boolean deletar(Lugar lugar) {
+        String sql = "DELETE FROM Lugar WHERE Place_ID = ?";
+
+        try {
+            PreparedStatement stmt = conn.prepareStatement(sql);
+
+            stmt.setInt(1, lugar.getPlace_id());
+
+            stmt.executeUpdate();
+
+            stmt.close();
+            conn.close();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            return false;
+        }
+
+        return true;
+    }
+
+    public List<Lugar> getLugaresByUsuario(String emailUsuario) {
+        String sql = "SELECT Place_Id, Descricao, Nome, Lat, Lgn FROM Lugar WHERE EmailUsuario ILIKE ?;";
+        List<Lugar> lugares = new ArrayList<>();
+
+        try {
+            PreparedStatement stmt = conn.prepareStatement(sql);
+
+            stmt.setString(1, emailUsuario);
+
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                Lugar lugar = new Lugar(
+                        rs.getInt("Place_Id"),
+                        rs.getString("Descricao"),
+                        rs.getString("Nome"),
+                        rs.getFloat("Lat"),
+                        rs.getFloat("Lng"),
+                        emailUsuario
+                );
+
+                lugares.add(lugar);
+            }
+
+            rs.close();
+            stmt.close();
+            conn.close();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+        return lugares;
+    }
 
 }
