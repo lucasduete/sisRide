3bcc874d7a99b46f720f27c77ef612dc4c5720ab
caioguilherme10 <caioguilhermeuna@gmail.com>
2018-02-26 07:40:07 +0000

    nada

--- src/main/java/io/github/sisRide/factory/Conexao.java
+++ src/main/java/io/github/sisRide/factory/Conexao.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class Conexao {
     private static final String url = "jdbc:postgresql://localhost:5432/sisRide";
     private static final String usuario = "postgres";
-    private static final String senha = "postgres";
+    private static final String senha = "caio0604";
     
     public static Connection getConnection() throws SQLException, ClassNotFoundException{
         Class.forName("org.postgresql.Driver");
--- src/main/java/io/github/sisRide/factory/DaoFactoryPostgres.java
+++ src/main/java/io/github/sisRide/factory/DaoFactoryPostgres.java
@@ -7,6 +7,7 @@ import io.github.sisRide.interfaces.DaoFactoryInterface;
 
 public class DaoFactoryPostgres implements DaoFactoryInterface {
 
+    @Override
     public UsuarioDaoInterface criaDaoUsuario() throws SQLException, ClassNotFoundException {
         return new UsuarioDaoPostgres();
     }
