260752e29e09ebc72361a3a0de1a1b478d08a19e
Lucas Duete <lm.duete@gmail.com>
2018-03-18 17:15:55 -0300

    Adicionando Método de Conexão com o Neo4j em Conexao.java

--- src/main/java/io/github/sisRide/factory/Conexao.java
+++ src/main/java/io/github/sisRide/factory/Conexao.java
@@ -1,5 +1,9 @@
 package io.github.sisRide.factory;
 
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+
+import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -8,10 +12,17 @@ public class Conexao {
     private static final String url = "jdbc:postgresql://localhost:5432/sisRide";
     private static final String usuario = "postgres";
     private static final String senha = "postgres";
+
+    private static final File BANCONEO4J = new File("sisRide.db");
     
     public static Connection getConnection() throws SQLException, ClassNotFoundException{
         Class.forName("org.postgresql.Driver");
         return DriverManager.getConnection(url, usuario, senha);
+    }
+
+    public static GraphDatabaseService getConnectionNeo4j() {
+        GraphDatabaseService graphDB = new GraphDatabaseFactory().newEmbeddedDatabase(BANCONEO4J);
+        return graphDB;
     }
 
 }
