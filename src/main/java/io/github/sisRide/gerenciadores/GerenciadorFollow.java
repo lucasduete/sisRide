ec68adbe509e3cb30e6d2bd40069b2c8c72eda80
Lucas Duete <lm.duete@gmail.com>
2018-03-20 11:00:14 -0300

    Refatorando GerenciadorFollow para Utilizar o Dao de Neo4j e Removendo Dependencias Desnecessárias e Remove do Logger.

--- src/main/java/io/github/sisRide/gerenciadores/GerenciadorFollow.java
+++ src/main/java/io/github/sisRide/gerenciadores/GerenciadorFollow.java
@@ -1,51 +1,48 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package io.github.sisRide.gerenciadores;
-
 
 import io.github.sisRide.dao.interfaces.FollowDaoInterface;
 import io.github.sisRide.factory.DaoFactory;
 import io.github.sisRide.interfaces.DaoFactoryInterface;
 import io.github.sisRide.model.Follow;
+import io.github.sisRide.model.Usuario;
+
 import java.sql.SQLException;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
-/**
- *
- * @author caio
- */
 public class GerenciadorFollow {
     
     private DaoFactoryInterface fabrica = null;
     private FollowDaoInterface followDao = null;
 
     public GerenciadorFollow() {
-        fabrica = DaoFactory.criarFabricaPostgres();
+        fabrica = DaoFactory.criarFabricaNeo4j();
+
         try {
             followDao = fabrica.criaDaoFollow();
+
         } catch (SQLException | ClassNotFoundException ex) {
-            Logger.getLogger(GerenciadorUsuario.class.getName()).log(Level.SEVERE, null, ex);
+
+            ex.printStackTrace();
         }
     }
     
-    public boolean salvar(Follow follow){
+    public boolean salvar(Follow follow) {
+
         return followDao.salvar(follow);
     }
     
-    public List<Follow> listar(){
+    public List<Usuario> listar() {
+
         return followDao.listar();
     }
     
-    public boolean deletar(Follow follow){
+    public boolean deletar(Follow follow) {
+
         return followDao.deletar(follow);
     }
     
-    public List<Follow> getFollowersByUsuario(String emailUsuario){
+    public List<Usuario> getFollowersByUsuario(String emailUsuario) {
+
         return followDao.getFollowersByUsuario(emailUsuario);
     }
 }
