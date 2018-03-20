16b94f51d971c35c73899a884381b37635e849f5
Lucas Duete <lm.duete@gmail.com>
2018-03-20 11:04:09 -0300

    Refatorado GerenciadorFriendship para Remover Logger e Utilizar Dao de Neo4j

--- src/main/java/io/github/sisRide/gerenciadores/GerenciadorFriendship.java
+++ src/main/java/io/github/sisRide/gerenciadores/GerenciadorFriendship.java
@@ -1,35 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package io.github.sisRide.gerenciadores;
-
 
 import io.github.sisRide.dao.interfaces.FriendshipDaoInterface;
 import io.github.sisRide.factory.DaoFactory;
 import io.github.sisRide.interfaces.DaoFactoryInterface;
 import io.github.sisRide.model.Friendship;
+import io.github.sisRide.model.Usuario;
+
 import java.sql.SQLException;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
-/**
- *
- * @author caio
- */
 public class GerenciadorFriendship {
     
     private DaoFactoryInterface fabrica = null;
     private FriendshipDaoInterface friendshipDao = null;
 
     public GerenciadorFriendship() {
-        fabrica = DaoFactory.criarFabricaPostgres();
+        fabrica = DaoFactory.criarFabricaNeo4j();
+
         try {
             friendshipDao = fabrica.criaDaoFriendship();
+
         } catch (SQLException | ClassNotFoundException ex) {
-            Logger.getLogger(GerenciadorUsuario.class.getName()).log(Level.SEVERE, null, ex);
+            ex.printStackTrace();
         }
     }
     
@@ -37,7 +29,7 @@ public class GerenciadorFriendship {
         return friendshipDao.salvar(friendship);
     }
     
-    public List<Friendship> listar(){
+    public List<Usuario> listar(){
         return friendshipDao.listar();
     }
     
@@ -45,7 +37,7 @@ public class GerenciadorFriendship {
         return friendshipDao.deletar(friendship);
     }
     
-    public List<Friendship> getAmigosByUsuario(String emailUsuario){
+    public List<Usuario> getAmigosByUsuario(String emailUsuario){
         return friendshipDao.getAmigosByUsuario(emailUsuario);
     }
     
