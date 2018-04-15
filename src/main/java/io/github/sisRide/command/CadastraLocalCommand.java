a0a3f80307e81a15667a9942624e2a918d91da86
Lucas Duete <lm.duete@gmail.com>
2018-04-14 21:59:01 -0300

    Implementando Command para cadastro de locais

---
+++ src/main/java/io/github/sisRide/command/CadastraLocalCommand.java
@@ -0,0 +1,43 @@
+package io.github.sisRide.command;
+
+import io.github.sisRide.gerenciadores.GerenciadorLugar;
+import io.github.sisRide.interfaces.Command;
+import io.github.sisRide.model.Lugar;
+import io.github.sisRide.model.Usuario;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CadastraLocalCommand implements Command {
+
+    public void CadastraLocalCommand() {
+
+    }
+
+    @Override
+    public void execute(HttpServletRequest req, HttpServletResponse res)
+            throws IOException, ServletException {
+
+        GerenciadorLugar gerenciadorLugar = new GerenciadorLugar();
+
+        Lugar lugar = new Lugar();
+        Usuario user = (Usuario) req.getSession().getAttribute("Usuario");
+
+        lugar.setNome(req.getParameter("nome"));
+        lugar.setDescricao(req.getParameter("descricao"));
+        lugar.setLat(Float.parseFloat(req.getParameter("lat")));
+        lugar.setLng(Float.parseFloat(req.getParameter("lng")));
+        lugar.setEmailUsuario(user.getEmail());
+
+        if (gerenciadorLugar.salvar(lugar)) {
+            res.setStatus(200);
+            res.sendRedirect("CadastraLocal.jsp?code=1");
+        } else {
+            res.setStatus(400);
+            res.sendRedirect("CadastraLocal.jsp?code=2");
+        }
+
+    }
+}
