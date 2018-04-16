5f8cd1981c088f30bb1c0713af77f556509034a6
Lucas Duete <lm.duete@gmail.com>
2018-04-16 11:48:12 -0300

    Implementando Controller Caronas Ofertadas.

---
+++ src/main/java/io/github/sisRide/command/CaronasOfertadasCommand.java
@@ -0,0 +1,34 @@
+package io.github.sisRide.command;
+
+import io.github.sisRide.gerenciadores.GerenciadorViagem;
+import io.github.sisRide.interfaces.Command;
+import io.github.sisRide.model.Viagem;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+public class CaronasOfertadasCommand implements Command {
+
+    @Override
+    public void execute(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+
+        GerenciadorViagem gerenciadorViagem = new GerenciadorViagem();
+
+        String localSaida = request.getParameter("localSaida");
+        String localChegada = request.getParameter("localChegada");
+        List<Viagem> viagens = gerenciadorViagem.searchViagem(localSaida, localChegada);
+
+        request.setAttribute(
+                "viagens", viagens
+        );
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("CaronasOfertadas.jsp");
+        dispatcher.forward(request, response);
+
+    }
+}
