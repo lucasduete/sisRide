bab7bfe7a89f1a8a4ea982feb73e759af76dfca5
Lucas Duete <lm.duete@gmail.com>
2018-04-16 12:52:18 -0300

    Implementando Método que Rejeita Solicitações

---
+++ src/main/java/io/github/sisRide/command/RejeitaSolicitacaoVagaCommand.java
@@ -0,0 +1,38 @@
+package io.github.sisRide.command;
+
+import io.github.sisRide.gerenciadores.GerenciadorReservaVaga;
+import io.github.sisRide.gerenciadores.GerenciadorSolicitacaoVaga;
+import io.github.sisRide.interfaces.Command;
+import io.github.sisRide.model.vaga.ReservaVaga;
+import io.github.sisRide.model.vaga.SolicitacaoVaga;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class RejeitaSolicitacaoVagaCommand implements Command {
+
+    @Override
+    public void execute(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+
+        GerenciadorSolicitacaoVaga solicitacaoVaga = new GerenciadorSolicitacaoVaga();
+
+        int idViagem = Integer.parseInt(request.getParameter("idViagem"));
+        String emailPassageiro = request.getParameter("emailPassageiro");
+
+        SolicitacaoVaga solicitacao = new SolicitacaoVaga();
+        solicitacao.setIdViagem(idViagem);
+        solicitacao.setEmailPassageiro(emailPassageiro);
+
+        if (solicitacaoVaga.deletar(solicitacao)) {
+            response.setStatus(200);
+            response.sendRedirect("CaronasOfertadas.jsp?code=1");
+        } else {
+            response.setStatus(400);
+            response.sendRedirect("CaronasOfertadas.jsp?code=2");
+        }
+
+    }
+}
