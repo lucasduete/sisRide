1a2aab2cabff9411aa08616432dd2f48308ffaa8
Lucas Duete <lm.duete@gmail.com>
2018-04-18 00:14:55 -0300

    Injetando filtro na aplicação.

--- src/main/java/io/github/sisRide/filters/AuthenticationFilter.java
+++ src/main/java/io/github/sisRide/filters/AuthenticationFilter.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebFilter(filterName = "Authentication")
+@WebFilter(filterName = "Authentication", servletNames = "FrontController")
 public class AuthenticationFilter implements Filter {
 
     private ServletContext servletContext;
