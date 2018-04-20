fdde1db68ba5b82fbf7a7e3756a950f98c3d6141
Lucas Duete <lm.duete@gmail.com>
2018-04-20 19:05:30 -0300

    Refatorando gerenciadores.

--- src/main/java/io/github/sisRide/gerenciadores/GerenciadorRequestFriendship.java
+++ src/main/java/io/github/sisRide/gerenciadores/GerenciadorRequestFriendship.java
@@ -1,10 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package io.github.sisRide.gerenciadores;
-
 
 import io.github.sisRide.dao.interfaces.RequestFriendshipDaoInterface;
 import io.github.sisRide.factory.DaoFactory;
@@ -15,10 +9,6 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- *
- * @author caio
- */
 public class GerenciadorRequestFriendship {
     
     private DaoFactoryInterface fabrica = null;
